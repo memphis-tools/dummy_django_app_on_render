02274bbf24f75ff2c730eaff443d42ef1945108b
memphis-tools <rn@github.com>
2024-08-16 22:01:25 +0200

    update init_app since Render not find media files

--- docker-compose-deployment.sh
+++ docker-compose-deployment.sh
@@ -71,15 +71,6 @@ else
       python manage.py collectstatic --no-input --clear
       export IS_TESTING=False
       python manage.py init_app
-      echo "DEBUG TIME SIR"
-      ls -l
-      ls -l media
-      realpath media/billet_1.jpg
-      mkdir media_things
-      cp -rap media/ media_things
-      ls -l media_things
-      id render
-      systemctl list-unit-files -t service
       gunicorn dummy_django_blog.wsgi:application --bind 0.0.0.0:8000
       ;;
     "down_dev" )
