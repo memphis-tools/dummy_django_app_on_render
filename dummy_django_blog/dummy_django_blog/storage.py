49ca2320c892b87662d04253fe785b44b6b67e13
memphis-tools <rn@github.com>
2024-08-21 17:00:37 +0200

    Deploy on Render with legacy STATICFILES_STORAGE

--- dummy_django_blog/dummy_django_blog/storage.py
+++
@@ -1,9 +0,0 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-class CustomStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    def _open(self, name, mode='rb'):
-        try:
-            return super()._open(name, mode)
-        except FileNotFoundError:
-            # Handle missing file gracefully, for example by returning None
-            return None
