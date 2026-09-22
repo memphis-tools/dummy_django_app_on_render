077945b6fcd5211536600e1b40d95b22098622be
memphis-tools <rn@github.com>
2026-09-22 12:50:55 +0200

    resolves enhance/ask-lumo-for-pretty-base-html

--- docker-compose-deployment.sh
+++ docker-compose-deployment.sh
@@ -14,7 +14,7 @@ function display_arg_error {
 }
 
 function update_application {
-  export IS_TESTING=False
+  export IS_TESTING=True
   docker compose -f docker-compose.dev.yml exec django python manage.py makemigrations authentication --noinput
   docker compose -f docker-compose.dev.yml exec django python manage.py makemigrations blog --noinput
   docker compose -f docker-compose.dev.yml exec django python manage.py migrate --noinput
